86ba36b23f5264af7997adfb9b54c997799c44c8
TobiasPleyer <tobi.pleyer@gmail.com>
2018-03-20 22:51:48 +0100

    Move Html parsing functions in their own module

--- src/Chefkoch/Html.hs
+++ src/Chefkoch/Html.hs
@@ -1,76 +1,21 @@
 module Chefkoch.Html where
 
 
-import Control.Monad
-import Data.Char
-import Data.List hiding (groupBy)
 import qualified Data.Text as T
 import Text.HTML.TagSoup
-import Text.StringLike
 
 import Chefkoch.DataTypes
 import Chefkoch.DataFunctions
 import Chefkoch.Http
 import Chefkoch.Html.Util
-
-
-extractRecipeTable :: [Tag T.Text] -> [Tag T.Text]
-extractRecipeTable = takeWhile (~/= TagClose "table")
-                   . tail
-                   . dropWhile (~/= TagOpen "table" [("class", "table-day")])
-
-
-extractRecipeInfo :: T.Text -> [[T.Text]]
-extractRecipeInfo = map (map unTag)
-                  . subGroups 4
-                  . filter (\t -> notEmptyText t
-                               && isNeeded t)
-                  . normalize
-                  . extractRecipeTable
-                  . parseTags
-    where
-      unTag :: Tag T.Text -> T.Text
-      unTag (TagText t) = t
-      unTag tag@(TagOpen aTag attrs) = fromAttrib (T.pack "href") tag
-      unTag _ = T.empty
-
-      isNeeded :: Tag T.Text -> Bool
-      isNeeded (TagOpen  t _)
-        | t == T.pack "tr"   = False
-        | t == T.pack "td"   = False
-        | t == T.pack "span" = False
-      isNeeded (TagClose t)
-        | t == T.pack "tr"   = False
-        | t == T.pack "td"   = False
-        | t == T.pack "span" = False
-        | t == T.pack "a"    = False
-      isNeeded _               = True
-
-
-extractCookingInstructions :: T.Text -> ([String], String)
-extractCookingInstructions website = (ingredients,instructions)
-  where
-    tags = parseTags website
-    ingredients = ( map (T.unpack . T.unwords . map fromTagText . filter isTagText)
-                  . groupBy "tr"
-                  . convertFraction
-                  . filter notEmptyText
-                  . normalize
-                  . takeWhile (~/= "</table>")
-                  . tail
-                  . dropWhile (~/= "<table class=incredients>")) tags
-    instructions = ( T.unpack
-                   . innerText
-                   . normalize
-                   . takeWhile (~/= "</div>")
-                   . dropWhile (~/= "<div id=rezept-zubereitung>")) tags
+import Chefkoch.Html.Parser
 
 
 mkRecipe year month [day, weekday, relative_url, name] = do
   let base_url = "https://www.chefkoch.de"
       recipe_url = base_url ++ (T.unpack relative_url)
   webcontent <- wgetURL recipe_url
-  let (ingredients,instructions) = extractCookingInstructions webcontent
+  let (ingredients,instructions) = parseCookingInstructions webcontent
   return (Recipe
            (read (T.unpack (T.init day)))
            (str2Weekday (T.unpack weekday))
@@ -85,5 +30,5 @@ mkRecipe year month [day, weekday, relative_url, name] = do
 fetchRecipes :: Year -> Month -> IO [Recipe]
 fetchRecipes year month = do
     webcontent <- fetchRecipeListing wgetURL year month
-    let raw_info = extractRecipeInfo webcontent
+    let raw_info = parseRecipeInfo webcontent
     mapM (mkRecipe year month) raw_info
---
+++ src/Chefkoch/Html/Parser.hs
@@ -0,0 +1,59 @@
+module Chefkoch.Html.Parser where
+
+
+import qualified Data.Text as T
+import Text.HTML.TagSoup
+
+import Chefkoch.Html.Util
+
+
+parseRecipeTable :: [Tag T.Text] -> [Tag T.Text]
+parseRecipeTable = takeWhile (~/= TagClose "table")
+                   . tail
+                   . dropWhile (~/= TagOpen "table" [("class", "table-day")])
+
+
+parseRecipeInfo :: T.Text -> [[T.Text]]
+parseRecipeInfo = map (map unTag)
+                  . subGroups 4
+                  . filter (\t -> notEmptyText t
+                               && isNeeded t)
+                  . normalize
+                  . parseRecipeTable
+                  . parseTags
+    where
+      unTag :: Tag T.Text -> T.Text
+      unTag (TagText t) = t
+      unTag tag@(TagOpen aTag attrs) = fromAttrib (T.pack "href") tag
+      unTag _ = T.empty
+
+      isNeeded :: Tag T.Text -> Bool
+      isNeeded (TagOpen  t _)
+        | t == T.pack "tr"   = False
+        | t == T.pack "td"   = False
+        | t == T.pack "span" = False
+      isNeeded (TagClose t)
+        | t == T.pack "tr"   = False
+        | t == T.pack "td"   = False
+        | t == T.pack "span" = False
+        | t == T.pack "a"    = False
+      isNeeded _               = True
+
+
+parseCookingInstructions :: T.Text -> ([String], String)
+parseCookingInstructions website = (ingredients,instructions)
+  where
+    tags = parseTags website
+    ingredients = ( map (T.unpack . T.unwords . map fromTagText . filter isTagText)
+                  . groupBy "tr"
+                  . convertFraction
+                  . filter notEmptyText
+                  . normalize
+                  . takeWhile (~/= "</table>")
+                  . tail
+                  . dropWhile (~/= "<table class=incredients>")) tags
+    instructions = ( T.unpack
+                   . innerText
+                   . normalize
+                   . takeWhile (~/= "</div>")
+                   . dropWhile (~/= "<div id=rezept-zubereitung>")) tags
